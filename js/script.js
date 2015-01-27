04f2a63e8a68ad1ce07cf6f0c41cfa59f4f8b98b
alejandro <alejozu9@gmail.com>
2015-01-27 10:18:13 -0600

    Terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// Javascript Code.
+var app = angular.module("Canciones", []);
+app.filter("Url", function(){
+	return function(item){
+		return (JSON.stringify(item)).slice(10,-18);
+	};
+});
+app.controller("CancionesController", function ($scope, $http) {
+	$http.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=545ac8adf6966995b1e9ea3a45d76d7d&format=json').success(function (data) {
+		$scope.datos = data.tracks.track;
+	});
+	$scope.ordenarPor = function(orden) {
+		$scope.ordenSeleccionado = orden;
+	};
+});
